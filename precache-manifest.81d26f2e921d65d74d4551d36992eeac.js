88f98cdbaf01456787829968c0489737b04db165
saurav <saurav@umbrella.qa>
2020-07-26 00:46:28 +0530

    Updates

---
+++ precache-manifest.81d26f2e921d65d74d4551d36992eeac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4226d17c114c1a171114824227fddd94",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "a358f9b8eb2b042ecb3f",
+    "url": "/aboutme/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "24162e55024d3d6b0170",
+    "url": "/aboutme/static/css/main.9beaa1ac.chunk.css"
+  },
+  {
+    "revision": "a358f9b8eb2b042ecb3f",
+    "url": "/aboutme/static/js/2.c19ff66a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/aboutme/static/js/2.c19ff66a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24162e55024d3d6b0170",
+    "url": "/aboutme/static/js/main.e008f87d.chunk.js"
+  },
+  {
+    "revision": "955f3070b93d413f0fd9",
+    "url": "/aboutme/static/js/runtime-main.95483440.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aboutme/static/media/logo.5d5d9eef.svg"
+  }
+]);
